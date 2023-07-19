9e4a182cc856ef39b79540aa81754937d52b5cf7
adamKellyObare <aobarekelly@gmail.com>
2023-07-19 16:12:16 +0300

    Done

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+// src/Article.jsx
+import React from "react";
+
+function Article({ title, date, preview }) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
